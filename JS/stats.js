c5fdcce98dd1a7b0b3640a188eee796f3286b447
Olof Ericsson <olof.ericsson@elev.ga.ntig.se>
2026-01-05 20:18:34 +0100

    Allt är klappat och klart

--- JS/stats.js
+++ JS/stats.js
@@ -1,6 +1,22 @@
 const canvas = document.getElementById('myChart');
 const ctx = canvas.getContext('2d');
 
+var stars = document.querySelector('#stars');
+var starCtx = stars.getContext('2d');
+
+const rect = stars.getBoundingClientRect();
+stars.width = rect.width;
+stars.height = rect.height;
+
+starCtx.fillStyle = "white";
+for (let i = 0; i < 100; i++) {
+  starCtx.fillRect(Math.floor(Math.random() * rect.width), Math.floor(Math.random() * rect.height), 2, 2);
+}
+starCtx.fillStyle = "blue";
+for (let i = 0; i < 1000; i++) {
+  starCtx.fillRect(Math.floor(Math.random() * rect.width), Math.floor(Math.random() * rect.height), 1, 1);
+}
+
 const chart = new Chart(ctx, {
   type: 'doughnut',
   data: {
@@ -17,6 +33,8 @@ const chart = new Chart(ctx, {
   }
 });
 
+//behöver fixas
 window.addEventListener('load', () => {
   chart.resize();
 });
+
